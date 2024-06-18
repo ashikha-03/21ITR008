a30adf758d62e764b2ef4e089ab397bfd454063b
Ashikha M <147821773+ashikha-03@users.noreply.github.com>
2024-06-18 11:04:05 +0530

    Create App.js

---
+++ Registration/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import RegistrationAndAuth from './RegistrationAndAuth';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>My App</h1>
+            </header>
+            <main>
+                <RegistrationAndAuth />
+            </main>
+        </div>
+    );
+}
+
+export default App;
